9256ad7475bf9cfb5f286ee01f289b1365e69dea
Igor Serko <igor.serko@gmail.com>
2016-03-20 22:46:41 +0000

    trying out webmock for testing

--- spec/inputs/cloudflare_spec.rb
+++ spec/inputs/cloudflare_spec.rb
@@ -1,11 +1,25 @@
 # encoding: utf-8
-require "logstash/devutils/rspec/spec_helper"
-require "logstash/inputs/cloudflare"
+require 'logstash/devutils/rspec/spec_helper'
+require 'logstash/inputs/cloudflare'
+require 'webmock/rspec'
+
+WebMock.disable_net_connect!(allow_localhost: true)
+
+RSpec.configure do |config|
+  config.before(:each) do
+    stub_request(:get, /api.cloudflare.com/)
+      .with(headers: { 'Accept' => '*/*', 'User-Agent' => 'Ruby' })
+      .to_return(status: 200, body: 'stubbed response', headers: {})
+  end
+end
 
 describe LogStash::Inputs::Cloudflare do
-
-  it_behaves_like "an interruptible input plugin" do
-    let(:config) { { "interval" => 100 } }
+  let(:config) do
+    {
+      'auth_email' => 'test@test.com',
+      'auth_key' => 'abcdefg',
+      'domain' => 'example.com'
+    }
   end
-
+  it_behaves_like 'an interruptible input plugin'
 end
