9127bc2b544c64881451d05ff830703e6d95a880
Igor Serko <igor.serko@gmail.com>
2016-08-08 09:47:12 +0100

    variable missing

--- lib/logstash/inputs/cloudflare.rb
+++ lib/logstash/inputs/cloudflare.rb
@@ -148,9 +148,9 @@ class LogStash::Inputs::Cloudflare < LogStash::Inputs::Base
     # if we have ray_id, we use that as a starting point
     if metadata['last_ray_id']
       @logger.info("Previous ray_id detected: #{ray_id}")
-      params['start_id'] = ray_id
+      params['start_id'] = metadata['last_ray_id']
       params['count'] = @batch_size
-      metadata['first_ray_id'] = ray_id
+      metadata['first_ray_id'] = metadata['last_ray_id']
       metadata['first_timestamp'] = nil
     elsif metadata['last_timestamp']
       dt_tstamp = DateTime.strptime(metadata['last_timestamp'], '%s')
